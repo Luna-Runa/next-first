f7dd7cbb12c257256cd32d8814370e18232f8890
Luna <91652033+Luna-Runa@users.noreply.github.com>
2022-04-19 20:04:44 +0900

    NavBar 생성, Components, styles jsx 학습

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: blue;
+        }
+      `}</style>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
