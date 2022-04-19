46b285e4cf3d62a623f86523a9404bfae84d7468
Luna <91652033+Luna-Runa@users.noreply.github.com>
2022-04-20 07:00:52 +0900

    eslint,  prettier 정리, Layout, Rewrite, SSR 학습

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: blue;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Title from "../components/Title";
 
-export default function Home() {
-  const [counter, setCounter] = useState(0);
+export default function Home({ results }: any) {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    <div className="container">
+      {results}
+      <Title title="Home" />
+      {!movies && <h4>로딩중...</h4>}
+      {movies?.map((movie: any) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+
+export function getServerSideProps() {
+  const { results } = { results: "abc" };
+  return {
+    props: { results },
+  };
 }
